48c12f8197bc4a004161394cf88458362444ba27
Tarek Magdi <tarekmagdi81@gmail.com>
2025-08-26 12:26:10 +0300

    Add Users module with initial configuration, routes, controllers, and views

---
+++ Modules/Users/App/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Users\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [];
+}
---
+++ Modules/Users/Routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Users\App\Http\Controllers\UsersController;
+
+Route::prefix('users')->as('users.')->controller(UsersController::class)->group(function () {
+    Route::group(['middleware' => ['auth:api']], function () {
+        //
+    });
+});
