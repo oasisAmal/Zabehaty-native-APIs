09e2de18bd69bc6a74bdf5d68f6ce5c7b5cabddf
Tarek Magdi <tarekmagdi81@gmail.com>
2025-09-03 17:40:34 +0300

    Implement OTP sending functionality in AuthService
    - Enhanced the sendOtp method to generate and send a verification code via SMS.
    - Added localization support for the OTP message in both English and Arabic language files.
    - Integrated SMSService for sending the OTP to the user's mobile number.

--- Modules/Auth/App/Services/AuthService.php
+++ Modules/Auth/App/Services/AuthService.php
@@ -2,8 +2,10 @@
 
 namespace Modules\Auth\App\Services;
 
+use App\Enums\Common;
 use Modules\Users\App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Services\Integrations\SMS\SMSService;
 
 class AuthService
 {
@@ -34,7 +36,17 @@ class AuthService
      */
     public function sendOtp($data)
     {
-        return $data;
+        $user = User::where('mobile', $data['mobile'])->first();
+        if (!$user) {
+            return false;
+        }
+
+        $user->otp_code = generateRandomNumber(Common::RANDOM_AUTH_CODE_LENGTH);
+        $user->otp_code_expire_at = now()->addMinutes(30);
+        $user->save();
+        $message = __('auth::messages.otp_code', ['code' => $user->otp_code]);
+
+        return app(SMSService::class)->send($message, $user->mobile, $data['mobile_country_code']);
     }
 
     /**
--- Modules/Auth/Lang/ar/messages.php
+++ Modules/Auth/Lang/ar/messages.php
@@ -4,6 +4,7 @@ return [
     'mobile_required' => 'الهاتف مطلوب',
     'mobile_country_code_required' => 'رمز الدولة مطلوب',
     'mobile_exists' => 'الهاتف غير موجود',
+    'otp_code' => 'رمز التحقق هو :code',
     'attributes' => [
         'mobile' => 'الهاتف',
         'mobile_country_code' => 'رمز الدولة',
