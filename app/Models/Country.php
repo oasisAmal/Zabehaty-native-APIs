1779bacdd0b0bf72c21f33faca3dbfedd37a6f2d
Tarek Magdi <tarekmagdi81@gmail.com>
2025-09-10 15:52:03 +0300

    Refactor country data handling in AppController and introduce Country model
    - Updated getMobileCountries method to utilize the new Country model for retrieving mobile-specific country data.
    - Adjusted the mapping of country attributes to align with the new model structure, including changes to country_code and mobile_code.
    - Added a new Country model to represent the countries table and support translatable attributes.

---
+++ app/Models/Country.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\TraitLanguage;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use TraitLanguage;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'countries';
+
+    /**
+     * The attributes that are translatable.
+     *
+     * @var array
+     */
+    protected $translatable = ['name'];
+}
