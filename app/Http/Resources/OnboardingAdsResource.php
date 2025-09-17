a8cacd80e545737444dda2fd3816fbb466a64557
Tarek Magdi <tarekmagdi81@gmail.com>
2025-09-17 10:00:04 +0300

    Refactor OnboardingAdsResource to retrieve onboarding ads from settings
    - Updated the OnboardingAdsResource to replace the parent toArray method with a custom implementation that fetches onboarding ads from the Settings model.
    - Introduced a private method, getOnboardingAds, to handle the retrieval and formatting of onboarding screen URLs, ensuring a more structured approach to data management.

--- app/Http/Resources/OnboardingAdsResource.php
+++ app/Http/Resources/OnboardingAdsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Settings;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,21 @@ class OnboardingAdsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return $this->getOnboardingAds();
+    }
+
+    private function getOnboardingAds()
+    {
+        $onboardingAds = Settings::where('key', 'on_boarding_screens')->first();
+        if ($onboardingAds == null || empty($onboardingAds->value)) {
+            return [];
+        }
+
+        $data = [];
+        foreach ($onboardingAds as $onboardingAd) {
+            $data[] = url('uploads/on-boarding-screens/' . $onboardingAd);
+        }
+        
+        return $data;
     }
 }
