c9d1c606e5e2ef0682bc4af53b8f64d830248dfc
Tarek Magdi <tarekmagdi81@gmail.com>
2025-09-19 11:58:27 +0300

    Remove 'id' attribute from AuthResource response array to streamline user data representation

--- Modules/Auth/App/Transformers/AuthResource.php
+++ Modules/Auth/App/Transformers/AuthResource.php
@@ -13,7 +13,6 @@ class AuthResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
