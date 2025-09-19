3e3331a5dd3dab2518c055d7ab7bf9c6b140deab
Tarek Magdi <tarekmagdi81@gmail.com>
2025-09-19 10:09:10 +0300

    Add PullCommand for Git operations and schedule it in app bootstrap
    - Introduced a new PullCommand to execute 'git pull' from the console, allowing for easy updates from the remote repository.
    - Scheduled the PullCommand to run every two minutes in the application bootstrap, automating the pull process.
    - Removed the previous '/git-pull' route to streamline Git operations within the command line interface.

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/git-pull', function () {
-    shell_exec("git status 2>&1");
-    $pull = shell_exec("git pull 2>&1");
-    return $pull;
-});
-
 Route::get('/migrate', function () {
     Artisan::call('migrate');
     return Artisan::output();
