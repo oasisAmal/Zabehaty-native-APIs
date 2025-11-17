1c35e2e93b4489c32c405d7e4beaf8cee57e8e17
Tarek Magdi <tarekmagdi81@gmail.com>
2025-11-17 15:17:46 +0200

    Add automatic eager loading and prevent lazy loading for models in production
    - Implemented automatic eager loading of relationships for all models to optimize performance.
    - Added prevention of lazy loading in production environments to enhance security and efficiency.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Automatically eager load relations for all models
+        Model::automaticallyEagerLoadRelationships();
+        
+        // Prevent Lazy Loading for all models in production
+        Model::preventLazyLoading(!app()->isProduction());
     }
 }
