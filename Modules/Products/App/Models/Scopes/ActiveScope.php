0689d746379ef711cbdc44c6b355b639ef57e847
Tarek Magdi <tarekmagdi81@gmail.com>
2025-11-19 15:43:25 +0200

    Comment out query logic in ActiveScope for future reference and improved readability

--- Modules/Products/App/Models/Scopes/ActiveScope.php
+++ Modules/Products/App/Models/Scopes/ActiveScope.php
@@ -10,16 +10,16 @@ class ActiveScope implements Scope
 {
     public function apply(Builder $builder, Model $model)
     {
-        return $builder->whereHas('department')
-            ->where('products.is_active', true)
-            ->where(function (Builder $q) {
-                $q->whereNull('products.shop_id')
-                ->orWhereHas('shop');
-            })
-            ->where('products.is_approved', true)
-            ->where(function (Builder $q) {
-                $q->where('products.price', '>', 0)
-                    ->orWhereHas('subProducts');
-            });
+        // return $builder->whereHas('department')
+        //     ->where('products.is_active', true)
+        //     ->where(function (Builder $q) {
+        //         $q->whereNull('products.shop_id')
+        //         ->orWhereHas('shop');
+        //     })
+        //     ->where('products.is_approved', true)
+        //     ->where(function (Builder $q) {
+        //         $q->where('products.price', '>', 0)
+        //             ->orWhereHas('subProducts');
+        //     });
     }
 }
