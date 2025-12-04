e85c535661e4729470733492a535a37061d801e2
Tarek Magdi <tarekmagdi81@gmail.com>
2025-12-04 07:08:13 +0200

    Update discountCalc function to set default values for parameters, ensuring robust calculations with zero as fallback for invalid inputs.

--- app/Helpers/MainHelpers.php
+++ app/Helpers/MainHelpers.php
@@ -168,7 +168,7 @@ function uniqueRandomCode($table, $col, $length = 16)
  *
  * @return float value
  */
-function discountCalc($old_price, $price)
+function discountCalc($old_price = 0, $price = 0)
 {
     if ($old_price > 0 && $price >= 0) {
         return round((($old_price - $price) * 100) / $old_price);
