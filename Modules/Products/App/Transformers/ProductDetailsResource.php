c1f5b3ef2974b5c1d56d19a3b416b48beeb51838
Tarek Magdi <tarekmagdi81@gmail.com>
2025-12-17 14:27:41 +0200

    Enhance ProductDetailsResource with additional shop and restaurant availability methods
    - Added methods to retrieve available shops and restaurants for a product based on its department, improving the API response with more comprehensive product availability information.
    - Updated the toArray method to include 'available_shops' and 'available_restaurants' fields, enhancing the detail provided in product responses.

--- Modules/Products/App/Transformers/ProductDetailsResource.php
+++ Modules/Products/App/Transformers/ProductDetailsResource.php
@@ -2,9 +2,12 @@
 
 namespace Modules\Products\App\Transformers;
 
-use App\Enums\CountryCurrencies;
 use Illuminate\Http\Request;
+use App\Enums\CountryCurrencies;
+use Illuminate\Support\Facades\DB;
+use Modules\Shops\App\Models\Shop;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Shops\App\Transformers\ShopCardResource;
 
 class ProductDetailsResource extends JsonResource
 {
@@ -18,8 +21,7 @@ class ProductDetailsResource extends JsonResource
             'name' => $this->name,
             'brief' => $this->brief,
             'description' => $this->description,
-            'thumbnail_url' => $this->thumb,
-            'image_url' => $this->image,
+            'image_url' => $this->image_url,
             'images' => $this->images,
             'shop' => $this->shop?->name,
             'category' => $this->category?->name,
@@ -30,10 +32,15 @@ class ProductDetailsResource extends JsonResource
             'limited_offer_expired_at' => $this->limited_offer_expired_at ? $this->limited_offer_expired_at->timestamp : null,
             'badge' => $this->badge_name ?? null,
             'is_favorite' => (bool) $this->is_favorite,
+            'has_gift' => $this->has_gift,
+            'allow_gift' => $this->allow_gift,
+            'allow_donate' => $this->allow_donate,
             'has_quantity' => $this->has_quantity,
             'quantity_settings' => $this->quantity_settings,
             'stock' => $this->stock_settings,
             'sizes' => $this->getSizes(),
+            'available_shops' => $this->availableShops(),
+            'available_restaurants' => $this->availableRestaurants(),
             'addon_sections' => AddonSectionResource::collection($this->addonSectionPivots ?? []),
         ];
     }
@@ -42,5 +49,36 @@ class ProductDetailsResource extends JsonResource
     {
         if (!$this->has_sub_products) return [];
         return SubProductResource::collection($this->subProducts);
+    }
+
+    private function availableShops()
+    {
+        if (!$this->department) return null;
+        $shops = $this->department->shops()
+            ->leftJoin('shop_products', function ($join) {
+                $join->on('shop_products.shop_id', '=', 'shops.id')
+                    ->on('shop_products.product_id', '=', DB::raw($this->id));
+            })
+            ->where('type', 'shop')
+            ->orderBy('shop_products.price')
+            ->get();
+        if ($shops->isEmpty() || $this->shop) return null;
+
+        return [
+            'section_name' => $this->department->shop_section_name,
+            'shops' => ShopCardResource::collection($shops),
+        ];
+    }
+    private function availableRestaurants()
+    {
+        if (!$this->department) return null;
+        $restaurants = Shop::where('type', 'restaurant')
+            ->whereHas('shopCookings', function ($q) {
+                $q->where('product_id', $this->id);
+            })
+            ->get();
+        if ($this->has_cookings == '0' || $this->productCookings->isEmpty() || $restaurants->isEmpty()) return null;
+
+        return ShopCardResource::collection($restaurants);
     }
 }
