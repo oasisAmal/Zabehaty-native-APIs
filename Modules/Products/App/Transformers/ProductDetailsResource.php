836b482ef96bd0631891b997c52420488533f6c1
Tarek Magdi <tarekmagdi81@gmail.com>
2025-12-17 13:02:25 +0200

    Add product details to ProductDetailsResource
    - Included additional fields in the toArray method for enhanced product information, such as image URL, shop name, category name, currency, price details, discount percentage, limited offer expiration, badge, and favorite status.
    - Improved the structure of the product details response for better API usability.

--- Modules/Products/App/Transformers/ProductDetailsResource.php
+++ Modules/Products/App/Transformers/ProductDetailsResource.php
@@ -16,6 +16,16 @@ class ProductDetailsResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'image_url' => $this->thumb,
+            'shop' => $this->shop?->name,
+            'category' => $this->category?->name,
+            'currency' => CountryCurrencies::getCurrency(),
+            'price' => (float) $this->price,
+            'price_before_discount' => (float) $this->old_price ?: null,
+            'discount_percentage' => (float) $this->discount_percentage ?: null,
+            'limited_offer_expired_at' => $this->limited_offer_expired_at ? $this->limited_offer_expired_at->timestamp : null,
+            'badge' => $this->badge_name ?? null,
+            'is_favorite' => (bool) $this->is_favorite,
             'addon_sections' => AddonSectionResource::collection($this->addonSectionPivots ?? []),
         ];
     }
