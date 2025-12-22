b4814011b0646806087bff30804164893be0b2d4
Tarek Magdi <tarekmagdi81@gmail.com>
2025-12-22 10:09:08 +0200

    Update Cart API routes to remove 'require-registered' middleware from product calculation endpoint

--- Modules/Cart/Routes/api.php
+++ Modules/Cart/Routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Cart\App\Http\Controllers\CartController;
 
 Route::prefix('cart')->as('cart.')->controller(CartController::class)->group(function () {
-    Route::group(['middleware' => ['auth:api', 'require-registered']], function () {
+    Route::group(['middleware' => ['auth:api']], function () {
         Route::post('calculate-product', 'calculateProduct');
     });
 });
