3806c3e225b39953eb2ac9fec4d063e3265dd34f
Tarek Magdi <tarekmagdi81@gmail.com>
2025-12-31 13:19:29 +0200

    Remove index view files from Categories, DynamicCategories, Products, and Shops modules

--- Modules/Categories/Resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-categories::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('categories.name') !!}</p>
-</x-categories::layouts.master>
--- Modules/Shops/Resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-shops::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('shops.name') !!}</p>
-</x-shops::layouts.master>
