b11f9c8bf9fe26e4e712dee9867e1bd386f93ee5
Tarek Magdi <tarekmagdi81@gmail.com>
2026-01-14 09:21:27 +0200

    Remove shop detail methods from ShopController and ShopsService
    - Deleted the detail method from ShopController, which handled shop detail retrieval and error responses.
    - Removed the getShopDetail method from ShopsService, simplifying the service by eliminating unused functionality.
    - Updated the API routes to remove the detail endpoint, streamlining the shop-related routes.

--- Modules/Shops/Routes/api.php
+++ Modules/Shops/Routes/api.php
@@ -5,7 +5,6 @@ use Modules\Shops\App\Http\Controllers\ShopController;
 
 Route::prefix('shops')->as('shops.')->controller(ShopController::class)->group(function () {
     Route::group(['middleware' => ['auth:api']], function () {
-        Route::get('/detail/{id}', 'detail');
         Route::post('/', 'index');
     });
 });
